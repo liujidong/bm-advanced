2254c02a380034218b7df625511f234276d8ffc3
liujidong <wwwliujidong@163.com>
2019-08-30 17:11:51 +0800

    创建字段

--- models/bug_stage.py
+++ models/bug_stage.py
@@ -6,6 +6,18 @@ class bugStage(models.Model):
     _name='bm.bug.stage'
     _description = 'bug阶段'
     _order='sequence,name'
-
+    #字符串有关类型
     name=fields.Char('名称')
+    desc_detail=fields.Text('描述')
+    status=fields.Selection([('waiting','未开始')('doing','进行中'),('closed','关闭'),('rework','重测未通过')],'状态')
+    document=fields.Html('文档')
+    #数值类型
     sequence=fields.Integer('Sequence')
+    percent_pro=fields.Float('进度',(3,2))
+    #日期类型
+    deadline=fields.Date('最晚解决日期')
+    create_on=fields.Datetime('创建时间',default=lambda self:fields.Datetime.now())
+    #布尔类型
+    delay=fields.Boolean('是否延误')
+    #二进制类型
+    image=fields.Binary('图片')
